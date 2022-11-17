08ddefa28889914b217b86d71664222d0091136f
jungwoo <hanjungwoo1@naver.com>
2022-11-17 23:43:57 +0900

    submit

--- KB/result_analysis.py
+++ KB/result_analysis.py
@@ -18,11 +18,15 @@ with open(open_path+"/log_evaluation_answer.txt", "r", encoding="utf-8") as f:
         if len(data) != 2:
             continue
 
-        gt = data[0]
-        pred = data[1]
+        gt = data[0].strip()
+        pred = data[1].strip()
 
         print(gt, pred)
 
+        if len(gt) != len(pred):
+            numeric_count += 1
 
 
+print("글자수 틀린 데이터 길이 : ", numeric_count)
+print("글자수 일치율 : ", numeric_count / count * 100)
 print("전체 데이터 길이 : ", count)
