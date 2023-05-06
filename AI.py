70490a5a851e05171d7e131ef1c4812c9cc65e9a
jungwoo <hanjungwoo1@naver.com>
2023-05-06 20:30:30 +0900

    submit

---
+++ AI.py
@@ -0,0 +1,23 @@
+# from diffusers import StableDiffusionPipeline
+# import torch
+#
+# model_id = 'dreamlike-art/dreamlike-pohtoreal-2.0'
+# pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+#
+# pipe = pipe.to('cuda')
+#
+# prompt = "annoying Student, man, tall, sporty"
+#
+# pipe(prompt).images[0]
+
+import openai
+openai.api_key = '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'
+
+response = openai.Image.create(
+  prompt="a white siamese cat",
+  n=1,
+  size="1024x1024"
+)
+image_url = response['data'][0]['url']
+
+print(image_url)
